1e0b7d521a9fd9e423fb2a9464ce853d5d6313df
ipa-rar <ragesh.ramachandran@ipa.fraunhofer.de>
2023-02-28 18:13:34 +0100

    added general repub

--- tf_merger/tf_repub.py
+++ tf_merger/tf_repub.py
@@ -3,13 +3,14 @@ from rclpy.node import Node
 from tf2_msgs.msg import TFMessage
 from rclpy.executors import SingleThreadedExecutor
 
+
 class TfRepublisher(Node):
 
     def __init__(self, namespace, agent):
-        super().__init__('tf_republisher_'+str(agent))
-        self.namespace=namespace
-        self.agent=agent
-        tf_topic=str(self.namespace)+'_'+str(self.agent)+'/tf'
+        super().__init__('tf_republisher_' + str(agent))
+        self.namespace = namespace
+        self.agent = agent
+        tf_topic = str(self.namespace) + '_' + str(self.agent) + '/tf'
         self.subscription = self.create_subscription(
             TFMessage,
             tf_topic,
@@ -23,28 +24,23 @@ class TfRepublisher(Node):
         )
 
     def tf_callback(self, msg):
-        tf_prefix=str(self.namespace)+'_'+str(self.agent)+'/'
+        tf_prefix = str(self.namespace) + '_' + str(self.agent) + '/'
         for transform in msg.transforms:
-            if transform.header.frame_id=='odom' and transform.child_frame_id=='base_link':
-                # Update the frame IDs to remove the barista_1 prefix
-                transform.header.frame_id = transform.header.frame_id.replace('odom', tf_prefix+'odom')
-                transform.child_frame_id = transform.child_frame_id.replace('base_link', tf_prefix+'base_link')
-            else:
-                # Update the frame IDs to remove the barista_1 prefix
-                transform.header.frame_id = transform.header.frame_id.replace('odom', tf_prefix+'odom')
-                transform.child_frame_id = transform.child_frame_id.replace('base_footprint', tf_prefix+'base_footprint')
-                           
+            # Update the frame IDs to add the namespace
+            transform.header.frame_id = tf_prefix + transform.header.frame_id
+            transform.child_frame_id = tf_prefix + transform.child_frame_id
         # Publish the transformed message on the '/tf' topic
         self.publisher.publish(msg)
+
 
 def main(args=None):
     rclpy.init(args=args)
     executor = SingleThreadedExecutor()
-    agents=4
-    tf_repubs=[]
-    try:  
-        for i in range(agents):   
-            tf_repub=TfRepublisher(namespace='barista', agent=i)
+    agents = 4
+    tf_repubs = []
+    try:
+        for i in range(agents):
+            tf_repub = TfRepublisher(namespace='barista', agent=i)
             tf_repubs.append(tf_repub)
         for tf_repub in tf_repubs:
             executor.add_node(tf_repub)
@@ -52,7 +48,7 @@ def main(args=None):
             executor.spin()
         finally:
             executor.shutdown()
-        
+
     except KeyboardInterrupt:
         executor.shutdown()
 
